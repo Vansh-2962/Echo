b38c20a4fc0e5293529ea7951fe30193efdf3237
Vansh singh sura <vanshsura29@gmail.com>
2026-05-02 22:10:02 +0530

    base url updated

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "https://echo-production-0d01.up.railway.app:3000",
+  baseURL: "https://echo-production-0d01.up.railway.app",
 });
