c1468a7f316bb0a988b5af5ced34d5c9515a3a0f
Vansh singh sura <vanshsura29@gmail.com>
2026-05-10 22:57:12 +0530

    Fix backend baseurl

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -6,8 +6,8 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma.js";
 
 export const auth = betterAuth({
-  // baseURL: `${process.env.BETTER_AUTH_URL}/api/auth`,
-  baseURL: `http://localhost:3000/api/auth`,
+  baseURL: `${process.env.BETTER_AUTH_URL}/api/auth`,
+  // baseURL: `http://localhost:3000/api/auth`,
 
   trustedOrigins: ["http://localhost:8080", "https://echo-fwq4.onrender.com"],
 
